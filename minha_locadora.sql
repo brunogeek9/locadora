488f92d5ad6a3f0f3400be8b4383a24d0962721f
brunogeek9 <brunogeek9@gmail.com>
2015-03-08 14:54:57 -0300

    banco da locadora atualizado

---
+++ minha_locadora.sql
@@ -0,0 +1,41 @@
+create database locadora;
+use locadora;
+create table cliente(
+	cpf_cliente int not null PRIMARY KEY,
+	nome varchar(40),
+	endereco varchar(40),
+	telefone int(11),
+	data_nasc date,
+	sexo char
+);
+create table emprestimo(
+	idEmprestimo int not null,
+	data_devolucao date,
+	data_emprestimo date,
+	data_prevista date,
+	idFilme int not null,
+	cpf_cliente int,
+	cpf_funcionario int,
+	PRIMARY KEY(idEmprestimo) 
+);
+create table funcionario(
+	cpf_funcionario int not null PRIMARY KEY,
+	nome varchar(40) not null,
+	fone int not null,
+	endereco varchar(40) not null
+);
+create table filme(
+	idFilme int not null PRIMARY KEY,
+	idGenero int not null,
+	nome varchar(40) not null,
+	duracao timestamp
+);
+create table genero(
+	idGenero int not null PRIMARY KEY,
+	nome varchar(40) not null
+);
+alter table filme add foreign key (idGenero) references genero (idGenero);
+alter table emprestimo add foreign key(idFilme) references filme(idFilme);
+alter table emprestimo add foreign key(cpf_cliente) references cliente(cpf_cliente);
+alter table emprestimo add foreign key(cpf_funcionario) references funcionario(cpf_funcionario);
+
